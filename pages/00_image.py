2e6adc11527be99a1037bf2cbbe6cab5e80b15d7
cham076 <lhs501@cham.sen.hs.kr>
2025-06-21 13:10:02 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,2 @@
+import. streamlit as st
+st.image("https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMm53bTl6eWs3dG1seWpvMXc1MmE1em50MGxicWlhdHdhM3gybGR5OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26gsspfbt1HfVQ9va/giphy.gif")
